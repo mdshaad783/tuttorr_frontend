d7a991cfaf0aca5861e772ed62763b38435ccd66
Md Shaad Rehan <113508620+mdshaad783@users.noreply.github.com>
2025-09-20 22:27:41 +0530

    Fixed AddAssignment.jsx

--- src/pages/Authority/AddAssignments.jsx
+++ src/pages/Authority/AddAssignments.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const AddAssignments = () => {
   return (
-    <div className="max-w-2xl mx-auto mt-10 bg-white p-6 rounded-lg shadow">
-  <h2 className="text-xl font-semibold mb-4">Add Assignment</h2>
-  <form className="space-y-4">
-    {/* Title */}
-    <input type="text" placeholder="Assignment Title" className="w-full border p-2 rounded" />
-    
-    {/* Description */}
-    <textarea placeholder="Description" className="w-full border p-2 rounded"></textarea>
-    
-    {/* Due Date & Time */}
-    <div className="flex gap-4">
-      <input type="date" className="border p-2 rounded w-1/2" />
-      <input type="time" className="border p-2 rounded w-1/2" />
+    <div className="max-w-2xl mx-auto mt-17 bg-white p-6 rounded-lg shadow md:mt-10">
+      <h2 className="text-xl font-semibold mb-4">Add Assignment</h2>
+      <form className="space-y-4">
+        {/* Title */}
+        <input
+          type="text"
+          placeholder="Assignment Title"
+          className="w-full border p-2 rounded"
+        />
+
+        {/* Description */}
+        <textarea
+          placeholder="Description"
+          className="w-full border p-2 rounded"
+        ></textarea>
+
+        {/* Due Date & Time */}
+        <div className="flex flex-row gap-2">
+          <label className="flex flex-col">
+            Deadline Date:
+            <input type="date" className="border p-2 rounded" />
+          </label>
+          <label className="flex flex-col">
+          Deadline time:
+          <input type="time" className="border p-2 rounded" />
+          </label>
+        </div>
+
+        {/* File Upload */}
+        <input type="file" className="w-full border p-2 rounded" />
+
+        <button className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">
+          Create Assignment
+        </button>
+      </form>
     </div>
+  );
+};
 
-    {/* Marks */}
-    <input type="number" placeholder="Maximum Marks" className="w-full border p-2 rounded" />
-
-    {/* File Upload */}
-    <input type="file" className="w-full border p-2 rounded" />
-
-    <button className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">
-      Create Assignment
-    </button>
-  </form>
-</div>
-
-  )
-}
-
-export default AddAssignments
+export default AddAssignments;
